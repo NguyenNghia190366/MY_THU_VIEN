b8b57c9186af89f5fda931815d3471763084c6d3
ONG GIA HUY <onggiahuy@gmail.com>
2025-07-20 14:07:37 +0700

    ✅ Implement Login feature

--- src/java/dao/UserDAO.java
+++ src/java/dao/UserDAO.java
@@ -46,6 +46,63 @@ public class UserDAO {
         }
         return user;
     }
+    
+    //--------------------------------- CHECK USER FOR REGISTER ---------------------------------
+    public User getUserByEmail(String email) {
+        try {
+            String sql = "SELECT * FROM users WHERE email = ?";
+            PreparedStatement ps = DBUtils.getConnection().prepareStatement(sql);
+            ps.setString(1, email);
+            ResultSet rs = ps.executeQuery();
+            if (rs.next()) {
+                return new User(
+                    rs.getInt("id"),
+                    rs.getString("name"),
+                    rs.getString("email"),
+                    rs.getString("password"),
+                    rs.getString("role"),
+                    rs.getString("status")
+                );
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+    
+    //--------------------------------- INSERT NEW REGISTER INTO DB ---------------------------------
+    public void register(User user) {
+        try {
+            String sql = "INSERT INTO users (name, email, password, role, status) VALUES (?, ?, ?, ?, ?)";
+            PreparedStatement ps = DBUtils.getConnection().prepareStatement(sql);
+            ps.setString(1, user.getName());
+            ps.setString(2, user.getEmail());
+            ps.setString(3, user.getPassword());
+            ps.setString(4, user.getRole());
+            ps.setString(5, user.getStatus());
+            ps.executeUpdate();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+    
+        //--------------------------------- UPDATE IN CHANGE PROFILE ---------------------------------
+
+    public boolean updateUser(User user) {
+    String sql = "UPDATE users SET name = ?, email = ?, password = ? WHERE id = ?";
+    try (Connection conn = DBUtils.getConnection();
+         PreparedStatement ps = conn.prepareStatement(sql)) {
+        ps.setString(1, user.getName());
+        ps.setString(2, user.getEmail());
+        ps.setString(3, user.getPassword());
+        ps.setInt(4, user.getId());
+        return ps.executeUpdate() > 0;
+    } catch (Exception e) {
+        e.printStackTrace();
+    }
+    return false;
+}
+
 
     public User checkUserByID(int id) {
         User user = null;
